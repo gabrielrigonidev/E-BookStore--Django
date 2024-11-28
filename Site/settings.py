645d8e301078cddd40ea0d335ec4f744cb5928c1
Gabriel Rigoni <rigonigabriel12@gmail.com>
2024-11-28 19:30:36 -0300

    vercel 4

--- Site/settings.py
+++ Site/settings.py
@@ -83,10 +83,9 @@ WSGI_APPLICATION = 'Site.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/tmp/db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
